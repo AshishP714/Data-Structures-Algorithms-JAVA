5b7e3012ba1a13eb1f12f5ebe931853a7218b7a5
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-06 22:40:49 +0530

    Add files via upload

---
+++ TwoSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,7,11,15};
+
+        int[] ans = twoSum(arr,9);
+
+        System.out.println(ans[0]+" "+ans[1]);
+    }
+}
