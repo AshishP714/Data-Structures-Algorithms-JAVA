b6856288304ab2a5cd4e97eb218aab7d9d658fc1
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-09 15:13:36 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+
+        char[] ch = str.toCharArray();
+
+        int left = 0;
+        int right = ch.length - 1;
+
+        while (left < right) {
+
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        System.out.println(new String(ch));
+    }
+}
