a4b8d9ecde9be802af45ceb3403dfe51a26d10f9
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-13 22:41:50 +0530

    Add files via upload

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] arr = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(arr));
+    }
+}
