ace0415290d188adc006b3d24158b87b5ca322ab
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-15 23:00:27 +0530

    Add files via upload

---
+++ DetectCycleInLinkedList.java
@@ -0,0 +1,31 @@
+public class DetectCycleInLinkedList {
+
+    static boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        head.next.next.next.next = head.next;
+
+        System.out.println(hasCycle(head));
+    }
+}
