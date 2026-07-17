a46a09cd06019fe4e1fa333fdfbde1203d522700
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-17 23:04:48 +0530

    Add files via upload

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,42 @@
+public class LevelOrderTraversal {
+
+    static void levelOrder(TreeNode root) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+                System.out.print(node.val + " ");
+
+                if (node.left != null)
+                    queue.offer(node.left);
+
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        levelOrder(root);
+    }
+}
