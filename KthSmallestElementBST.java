7767011f63a45ce66d97db64e9ee58e592ef8382
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-22 23:09:55 +0530

    Implement Kth smallest element in BST

---
+++ KthSmallestElementBST.java
@@ -0,0 +1,37 @@
+public class KthSmallestElementBST{
+
+    static int kthSmallest(TreeNode root,int k){
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        while(true){
+
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            if(--k == 0)
+                return root.val;
+
+            root = root.right;
+        }
+    }
+
+    public static void main(String[] args){
+
+        TreeNode root = new TreeNode(5);
+
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+
+        root.left.left.left = new TreeNode(1);
+
+        System.out.println(kthSmallest(root,3));
+    }
+}
