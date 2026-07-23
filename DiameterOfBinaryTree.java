1e18c0141d2eceaecd8c1d00b57617241c19e8d8
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-07-23 23:01:54 +0530

    Implement diameter calculation for binary tree
    Calculates the diameter of a binary tree using a recursive approach.

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,32 @@
+public class DiameterOfBinaryTree {
+
+    static int diameter = 0;
+
+    static int height(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        diameter = Math.max(diameter, left + right);
+
+        return Math.max(left, right) + 1;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        height(root);
+
+        System.out.println("Diameter = " + diameter);
+    }
+}
